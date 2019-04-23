b8796e5c64b649d03c9f03753db407c56bb48444
Steven Orvis <sorvis@creehan.com>
2019-04-23 09:11:53 -0400

    Fixed error

--- HideCategorizedOrphans.php
+++ HideCategorizedOrphans.php
@@ -27,33 +27,4 @@ $wgExtensionCredits['other'][] = array(
 $wgAutoloadClasses['HideCategorizedOrphansHooks'] = __DIR__ . '/HideCategorizedOrphans.hooks.php';
 
 // Register hooks
-$wgHooks['LonelyPagesQuery'][] = 'HideCategorizedOrphansHooks::onLonelyPagesQuery';<?php
-/**
-*  * HideCategoizedOrphans extension
-*   *
-*    * For more info see https://mediawiki.org/wiki/Extension:HideCategorizedOrphans
-*     *
-*      * @file
-*       * @ingroup Extensions
-*        * @author Steven Orvis, 2014
-*         * @license GNU General Public Licence 2.0 or later
-*          */
-
-$wgExtensionCredits['other'][] = array(
-	        'path' => __FILE__,
-		        'name' => 'HideCategorizedOrphans',
-			        'author' => array(
-					                'Steven Orvis',
-							        ),
-								        'version'  => '0.2.0',
-									        'url' => 'https://www.mediawiki.org/wiki/Extension:HideCategorizedOrphans',
-										        'descriptionmsg' => 'Hides orphaned pages that belong to a category',
-										);
-
-/* Setup */
-
-// Register files
-// $wgAutoloadClasses['HideCategorizedOrphansHooks'] = __DIR__ . '/HideCategorizedOrphans.hooks.php';
-//
-// // Register hooks
-// $wgHooks['LonelyPagesQuery'][] = 'HideCategorizedOrphansHooks::onLonelyPagesQuery';
+$wgHooks['LonelyPagesQuery'][] = 'HideCategorizedOrphansHooks::onLonelyPagesQuery';
